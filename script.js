8165d82045f9ec68e91cf4688b022ec7b021f512
Frantz Roulet <frantzroulet@gmail.com>
2015-08-01 12:51:35 +0200

    Major Update July 2015

--- script.js
+++ script.js
@@ -6,91 +6,144 @@
     Read more: http://feross.org/like-everything-on-facebook/
 */
 
-var nbexpands = 2;
-var count = 0;
-var message1 = 'Like this';
-var message2 = 'Like this comment';
+  var nbexpands = 2;
+  var count = 1;
+  var message1 = 'Like this';
+  var message2 = 'Like this comment';
 
-function expandComments (){
-  count ++;
 
-  //Click on all the like/comment box
-  [].forEach.call(document.querySelectorAll(".UFIBlingBox "), function(it){
-    if( !it.querySelector(".ufiPagerLoading") ){
-      it.click();
-    }
-  });
+  function init() {
+    /////////////MAIN LOGIC//////////////
+    //Creating the div with all the elements
+    var happyDiv = document.createElement('div');
+    happyDiv.innerHTML = '<div id=\'happy\' style=\'background-color:#ddd;font-size:16px;text-align:center;position:fixed;top:40px;right:40px;width:200px;height:100px;border:4px solid black;z-index:9999;padding-top:15px;\'><span>0</span> of <span id=\'counthappy\'>0</span> items liked.<div id=\'happyStatus\' style=\'margin-top:9px;\'><a id=\'happyButton\' href=\'#\' style=\'display:block;\' onclick=\'haltFn();\'> Stop it.</a></div><FORM><INPUT type=\'checkbox\' name=\'expands\' value=\'\'>Expand all comments</FORM><button type="button" id=\'but\'>Click Me!</button></div>';
+    document.getElementsByTagName('body')[0].appendChild(happyDiv);
 
-  //Expand comments (click on 'view more comments')
-  [].forEach.call(document.querySelectorAll(".UFIPagerLink "), function(it){
-    if( !it.querySelector(".ufiPagerLoading") ){
-      it.click();
-    }
-  });
+    /////ONCLICK/////
+    document.getElementById('but').addEventListener('click', function() {
 
-  // If We want to count the number of expands
-  if( count < nbexpands ){
-    window.setTimeout(expandComments, 3000);
+    removeElementsByClass('_4ikz');
+
+      //Open an wait a little to expand the comments
+      openComments();
+      setTimeout(expandComments, 2000);
+
+    }, false);
+
   }
 
-  /*        // If We want to expand ALL the comments
-  if( document.querySelectorAll(".UFIPagerLink ").length ){
-  window.setTimeout(expandComments, 3000);
-}*/
+  function openComments() {
 
-};
+    //Click on all the like/comment box
+    [].forEach.call(document.querySelectorAll(".UFIBlingBox "), function(it) {
+      if (!it.querySelector(".ufiPagerLoading")) {
+        it.click();
+      }
+    });
+    
+  };
 
+function expandComments(){
 
-function likeeverything(){
+  //Expand comments (click on 'view more comments') only once
+    [].forEach.call(document.querySelectorAll(".UFIPagerLink "), function(it) {
+      if (!it.querySelector(".ufiPagerLoading")) {
+        it.click();
+      }
+    });
 
-  //var sad = document.getElementsByTagName('*'),
-  var sad = document.getElementsByTagName('a'),
-  happy = [],
-  halt = false;
+    // If selected, expands ALL the comments
+    if(document.getElementsByName("expands")[0].checked && count > 0)
+    { 
 
-  // Select only the Like buttons.
-  // Convert the sad NodeList to a happy Array.
-  for (var i = 0; i < sad.length; i++) {
-    //BE CAREFUL WITH YOUR LANGUAGE HERE, CHANGE IT IF NECESSARY
-    if (sad[i] && (sad[i].title == message1 || sad[i].title == message2)) {
-      happy.push(sad[i]);
+        if( document.querySelectorAll(".UFIPagerLink ").length ){
+        document.body.style.overflow = '';
+        window.scrollTo(0, 0);
+        document.body.style.overflow = 'hidden'
+        document.getElementById('happyStatus').innerHTML = 'Expanding Comments ...';
+        window.setTimeout(expandComments, 3000);
+        removeElementsByClass('_4ikz');
+        }
+
+        else {
+        document.body.style.overflow = '';
+        document.getElementById('happyStatus').innerHTML = '';
+        likeeverything();
+        }
+
     }
-  }
 
-  var happyDiv = document.createElement('div');
-  happyDiv.innerHTML = '<div id=\'happy\' style=\'background-color:#ddd;font-size:16px;text-align:center;position:fixed;top:40px;right:40px;width:200px;height:100px;border:4px solid black;z-index:9999;padding-top:15px;\'><span>0</span> of '+happy.length+' items liked.<div id=\'happyStatus\' style=\'margin-top:30px;\'><a id=\'happyButton\' href=\'#\' style=\'display:block;\' onclick=\'haltFn();\'>Stop it.</a></div></div>';
-  document.getElementsByTagName('body')[0].appendChild(happyDiv);
-
-  function happyFn(happy) {
-    if (halt || !happy || !happy.length) {
-      document.getElementById('happyStatus').innerHTML = 'Done!';
-
-      /*     // Remove the happy box if necessary
-      var selectTag = document.getElementById('happy');
-      selectTag.parentNode.removeChild( selectTag);*/
-
-      return;
+    //If not, like everything already present
+    else {
+      document.getElementById('happyStatus').innerHTML = '';
+      likeeverything();
     }
-    happy[0].click();
-    happy[0].style.color='#FF0000';
-    var countSpan = document.querySelector('#happy span');
-    countSpan.innerHTML = parseInt(countSpan.innerHTML) + 1;
-
-    // Wait for each Like to be processed before trying the next.
-    // Facebook enforces this requirement.
-    window.setTimeout(function() {
-      happyFn(happy.splice(1));
-    }, 5000);
-  }
-
-  function haltFn() {
-    halt = true;
-    return false; // prevent default event
-  }
-
-  happyFn(happy);
 
 }
 
-expandComments();
-likeeverything();
+
+  function likeeverything() {
+    
+    //var sad = document.getElementsByTagName('*'),
+    var sad = document.getElementsByTagName('a'),
+      happy = [],
+      halt = false;
+
+    // Select only the Like buttons.
+    // Convert the sad NodeList to a happy Array.
+    for (var i = 0; i < sad.length; i++) {
+      //BE CAREFUL WITH YOUR LANGUAGE HERE, CHANGE IT IF NECESSARY
+      if (sad[i] && (sad[i].title == message1 || sad[i].title == message2)) {
+        happy.push(sad[i]);
+      }
+
+    }
+
+    // Put the number of items in the box.
+    var counthappy = document.querySelector('#counthappy');
+    counthappy.innerHTML = happy.length;
+
+
+function happyFn(happy) {
+      if (halt || !happy || !happy.length) {
+
+        document.getElementById('happyStatus').innerHTML = 'Done!';
+
+        /*     // Remove the happy box if necessary
+        var selectTag = document.getElementById('happy');
+        selectTag.parentNode.removeChild( selectTag);*/
+
+        return;
+
+      }
+      happy[0].click();
+      happy[0].style.color = '#FF0000';
+
+      var countSpan = document.querySelector('#happy span');
+      countSpan.innerHTML = parseInt(countSpan.innerHTML) + 1;
+
+      // Wait for each Like to be processed before trying the next.
+      // Facebook enforces this requirement.
+      window.setTimeout(function() {
+        happyFn(happy.splice(1));
+      }, 5000);
+    }
+
+    function haltFn() {
+      halt = true;
+      return false; // prevent default event
+    }
+
+    happyFn(happy);
+
+  }
+
+  function removeElementsByClass(className){
+var elements = document.getElementsByClassName(className);
+
+  while(elements.length > 3){
+      elements[0].parentNode.removeChild(elements[0]);
+  }
+}
+
+init();
