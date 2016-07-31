44a2a6fb327506ed2c2372db1f45b8c77ee03e69
Frantz Roulet <frantzroulet@gmail.com>
2016-07-31 10:40:50 +0200

    Like Friends's post only

--- script.js
+++ script.js
@@ -10,22 +10,31 @@
   var count = 1;
   var message1 = 'Like this';
   var message2 = 'Like this comment';
+  var exempendsallcomments = false;
+  var friendsonly = false;
+  var halt = false;
 
   function init() {
     /////////////MAIN LOGIC//////////////
     //Creating the div with all the elements
     var happyDiv = document.createElement('div');
-    happyDiv.innerHTML = '<div id=\'happy\' style=\'background-color:#ddd;font-size:16px;text-align:center;position:fixed;top:40px;right:40px;width:200px;height:100px;border:4px solid black;z-index:9999;padding-top:15px;\'><span>0</span> of <span id=\'counthappy\'>0</span> items liked.<div id=\'happyStatus\' style=\'margin-top:9px;\'><a id=\'happyButton\' href=\'#\' style=\'display:block;\' onclick=\'haltFn();\'> Stop it.</a></div><FORM><INPUT type=\'checkbox\' name=\'expands\' value=\'\'>Expand all comments</FORM><button type="button" id=\'but\'>Click Me!</button></div>';
+    happyDiv.innerHTML = '<div id=\'happy\' style=\'background-color:#ddd;font-size:16px;text-align:center;position:fixed;top:40px;right:40px;width:200px;height:150px;border:4px solid black;z-index:9999;padding-top:15px;\'><span>0</span> of <span id=\'counthappy\'>0</span> items liked.<div id=\'happyStatus\' style=\'margin-top:9px;\'><a id=\'happyButton\' href=\'#\' style=\'display:block;\' onclick=\'haltFn();\'> Stop it.</a></div><FORM><INPUT type=\'checkbox\' name=\'expands\' value=\'\'>Expand all comments <br> <INPUT type=\'checkbox\' name=\'fnonly\' value=\'\'>Only like friends post</FORM><button type="button" id=\'but\'>Click Me!</button></div>';
+
     document.getElementsByTagName('body')[0].appendChild(happyDiv);
 
-    /////ONCLICK///// 
+
+    /////ONCLICK/////
     document.getElementById('but').addEventListener('click', function() {
 
     removeDivsByClass('_4ikz');
 
-      //Open an wait a little to expand the comments
-      openComments();
-      setTimeout(expandComments, 2000);
+    friendsonly = document.getElementsByName("fnonly")[0].checked;
+    exempendsallcomments = document.getElementsByName("expands")[0].checked;
+
+    //Open an wait a little to expand the comments
+    openComments();
+    expandComments();
+
 
     }, false);
 
@@ -33,13 +42,23 @@
 
   function openComments() {
 
+    var boxes = document.querySelectorAll(".UFIBlingBox ");
+    if(boxes.length < 0 || boxes == null)
+    {
+      return;
+    }
+
     //Click on all the like/comment box
-    [].forEach.call(document.querySelectorAll(".UFIBlingBox "), function(it) {
+    [].forEach.call(boxes, function(it) {
       if (!it.querySelector(".ufiPagerLoading")) {
         it.click();
       }
     });
-    
+
+    setTimeout(function(){}, 2000);
+
+    return;
+
   };
 
 function expandComments(){
@@ -52,8 +71,8 @@ function expandComments(){
     });
 
     // If selected, expands ALL the comments
-    if(document.getElementsByName("expands")[0].checked && count > 0)
-    { 
+    if(exempendsallcomments && count > 0)
+    {
 
         if( document.querySelectorAll(".UFIPagerLink ").length ){
         document.body.style.overflow = '';
@@ -81,7 +100,16 @@ function expandComments(){
 }
 
   function likeeverything() {
-    
+
+    if(friendsonly)
+    {
+      happy = getAllFriendsPost();
+      var counthappy = document.querySelector('#counthappy');
+      counthappy.innerHTML = happy.length;
+      happyFn(happy);
+      return;
+    }
+
     //var sad = document.getElementsByTagName('*'),
     var sad = document.getElementsByTagName('a'),
       happy = [],
@@ -101,10 +129,16 @@ function expandComments(){
     var counthappy = document.querySelector('#counthappy');
     counthappy.innerHTML = happy.length;
 
+    happyFn(happy);
+}
+
 
 function happyFn(happy) {
-      if (halt || !happy || !happy.length) {
 
+      console.log("HAPPYFN :D");
+
+      if (!happy || !happy.length) {
+        console.log("BYE");
         document.getElementById('happyStatus').innerHTML = 'Done!';
 
         /*     // Remove the happy box if necessary
@@ -114,6 +148,8 @@ function happyFn(happy) {
         return;
 
       }
+
+      console.log("UI");
       happy[0].click();
       happy[0].style.color = '#FF0000';
 
@@ -125,16 +161,10 @@ function happyFn(happy) {
       window.setTimeout(function() {
         happyFn(happy.splice(1));
       }, 5000);
+
     }
 
-    function haltFn() {
-      halt = true;
-      return false; // prevent default event
-    }
 
-    happyFn(happy);
-
-  }
 
 function removeDivsByClass(className){
   var elements = document.getElementsByClassName(className);
@@ -142,6 +172,48 @@ function removeDivsByClass(className){
     while(elements.length > nbposts){
         elements[0].parentNode.removeChild(elements[0]);
     }
+}
+
+function getAllFriendsPost()
+{
+  console.log("GET ALL FRIENDS POST");
+  happy = [];
+  var allpost = document.querySelectorAll("[data-insertion-position]");
+
+  console.log(allpost.length);
+
+  [].forEach.call(allpost, function(post) {
+    console.log("OK");
+
+    var hovercard = post.querySelector("[data-hovercard]");
+    console.log(hovercard);
+
+    var lehref = hovercard.getAttribute("href");
+    console.log(lehref);
+
+    var myRegexp = /\?fref=nf$/g;
+    var match = myRegexp.exec(lehref);
+    console.log("LA REGEXP");
+
+    console.log(match);
+    //typeof obj.foo != 'undefined'
+    if(match == null)
+    {
+      console.log("No match");
+      return;
+    }
+
+    console.log("Le Match");
+    likelink = post.querySelector("[data-testid=fb-ufi-likelink]");
+    happy.push(likelink);
+
+    return;
+
+  });
+
+  console.log("DONE, NOW RETURN");
+  return happy;
+
 }
 
 init();
