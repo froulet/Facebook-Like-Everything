c9ead0534b0e56cfbe4e3e33bb817da10d1d6fbf
Frantz Roulet <frantzroulet@gmail.com>
2016-07-31 11:01:13 +0200

    Like everything ok

--- script.js
+++ script.js
@@ -18,23 +18,21 @@
     /////////////MAIN LOGIC//////////////
     //Creating the div with all the elements
     var happyDiv = document.createElement('div');
-    happyDiv.innerHTML = "<div id='happy' style='background-color:#ddd;font-size:16px;text-align:center;position:fixed;top:40px;right:40px;width:200px;height:150px;border:4px solid black;z-index:9999;padding-top:15px;'><span>0</span> of <span id='counthappy'>0</span> items liked.<div id='happyStatus' style='margin-top:9px;'><a id='happyButton' href='#' style='display:block;' onclick='haltFn();'> Stop it.</a></div><FORM><INPUT type='checkbox' name='expands' value=''>Expand all comments <br> <INPUT type='checkbox' name='fnonly' value=''>Only like friends post</FORM><button type="button" id='but'>Click Me!</button></div>";
+    happyDiv.innerHTML = "<div id='happy' style='background-color:#ddd;font-size:16px;text-align:center;position:fixed;top:40px;right:40px;width:200px;height:150px;border:4px solid black;z-index:9999;padding-top:15px;'><span>0</span> of <span id='counthappy'>0</span> items liked.<div id='happyStatus' style='margin-top:9px;'><a id='happyButton' href='#' style='display:block;' onclick='haltFn();'> Stop it.</a></div><FORM><INPUT type='checkbox' name='expands' value=''>Expand all comments <br> <INPUT type='checkbox' name='fnonly' value=''>Only like friends post</FORM><button type='button' id='but'>Click Me!</button></div>";
 
     document.getElementsByTagName('body')[0].appendChild(happyDiv);
 
-
     /////ONCLICK/////
     document.getElementById('but').addEventListener('click', function() {
-
-    removeDivsByClass('_4ikz');
-
+    //Checking parameters
     friendsonly = document.getElementsByName("fnonly")[0].checked;
     exempendsallcomments = document.getElementsByName("expands")[0].checked;
+
+    removeDivsByClass('_4ikz');
 
     //Open an wait a little to expand the comments
     openComments();
     expandComments();
-
 
     }, false);
 
@@ -112,14 +110,20 @@ function expandComments(){
 
     //var sad = document.getElementsByTagName('*'),
     var sad = document.getElementsByTagName('a'),
-      happy = [],
-      halt = false;
+      happy = [];
+
+    console.log(sad.length);
 
     // Select only the Like buttons.
     // Convert the sad NodeList to a happy Array.
     for (var i = 0; i < sad.length; i++) {
-      //BE CAREFUL WITH YOUR LANGUAGE HERE, CHANGE IT IF NECESSARY
-      if (sad[i] && (sad[i].getAttribute("aria-label") == message1 || sad[i].title == message2)) {
+
+      if(!sad[i]){continue;}
+
+      var datatest = sad[i].getAttribute("data-testid");
+
+      if (datatest == "fb-ufi-likelink" || datatest == "ufi_comment_like_link") {
+        console.log('one sad');
         happy.push(sad[i]);
       }
 
@@ -181,7 +185,6 @@ function getAllFriendsPost()
   console.log(allpost.length);
 
   [].forEach.call(allpost, function(post) {
-    console.log("OK");
 
     var hovercard = post.querySelector("[data-hovercard]");
     console.log(hovercard);
